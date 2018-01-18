23a3b138273807535a61c7e95c28b577502f722e
lenemarix <lenemarix@gmail.com>
2018-01-19 00:21:08 +0900

    Logging playing poker duration when statemachine stopped

--- src/main/java/com/github/lenemarix/autoplay/dq11/poker/statemachine/listener/LoggingStatemachineListener.java
+++ src/main/java/com/github/lenemarix/autoplay/dq11/poker/statemachine/listener/LoggingStatemachineListener.java
@@ -1,9 +1,14 @@
 package com.github.lenemarix.autoplay.dq11.poker.statemachine.listener;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.annotation.OnStateMachineStart;
+import org.springframework.statemachine.annotation.OnStateMachineStop;
 import org.springframework.statemachine.annotation.OnTransition;
 import org.springframework.statemachine.annotation.WithStateMachine;
 
@@ -18,6 +23,23 @@ public class LoggingStatemachineListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingStatemachineListener.class);
 
+    private LocalDateTime startTime;
+    
+    @OnStateMachineStart
+    public void onStateMachineStart() {
+        startTime = LocalDateTime.now();
+    }
+    
+    @OnStateMachineStop
+    public void onStateMachineStop() {
+        Duration duration = Duration.between(startTime, LocalDateTime.now());
+        long hours = duration.toHours();
+        long minutes = duration.toMinutes() - (hours * 60);
+        long seconds = duration.getSeconds() - (hours * 60 * 60) - (minutes * 60);
+
+        LOGGER.info("Playing poker duration: {}h:{}m:{}s ({})", hours, minutes, seconds, duration.toString());
+    }
+    
     @OnTransition
     public void onTransition(StateContext<States, Events> stateContext) {
         String eventId = stateContext.getMessageHeaders().get("eventId", String.class);
